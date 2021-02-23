91b54d171c6a185f67b38bc3f721ede79ca26a73
Asarua <1299458195@qq.com>
2021-02-23 09:40:29 +0800

    feat(all): 在vue3中尝试使用jsx的两种用法

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-});
+  setup() {
+    return () => (
+      <div>
+        <img alt='Vue logo' src={require('./assets/logo.png')} />
+        <HelloWorld msg='Welcome to Your Vue.js + TypeScript App' />
+      </div>
+    )
+  }
+})
 </script>
 
 <style>
